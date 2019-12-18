319e9989ad98472f0f259794a43500b5773d12da
Derrick Craven <dcravenus@gmail.com>
2019-12-18 08:52:57 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AwesomeSlider from 'react-awesome-slider'
+import 'react-awesome-slider/dist/styles.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AwesomeSlider>
+                <div>
+                    <iframe src="https://giphy.com/embed/y5GI8at3OFxnO" width="480" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </div>
+                <div>
+                    <iframe src="https://giphy.com/embed/zSSwv96d4pb8c" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </div>
+                <div>
+                    <iframe src="https://giphy.com/embed/uHBmHeA1IM4V2" width="480" height="267" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </div>
+            </AwesomeSlider>
+        </div>
+    );
 }
 
 export default App;
